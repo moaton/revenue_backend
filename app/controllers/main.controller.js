0d85d5366134c669850c58d3b8272c0af713524f
Anet Zhuban <moaton@mail.ru>
2022-07-16 21:56:00 +0600

    v2.1

--- app/controllers/main.controller.js
+++ app/controllers/main.controller.js
@@ -26,7 +26,7 @@ exports.create = async (req, res) => {
     } else {
       bcrypt.compare(req.body.password, resultsCash[0].password, async function(err, result) {
         if(result){
-          let data = await revenues.findOne({id: resultsCash[0].id})
+          let data = await revenues.findOne({where: {id: resultsCash[0].id} })
           res.send(data.dataValues)
         } else {
           res.status(404).send({message: 'the user has been created'})
@@ -39,13 +39,13 @@ exports.findAll = async (req, res) => {
   console.log(req, 'req2');
 };
 exports.findOne = async (req, res) => {
-  let data = await revenues.findOne({id: req.id})
+  let data = await revenues.findOne({where: {id: req.params.id}})
   res.send(data.dataValues)
 };
 exports.update = async (req, res) => {
   let data = null
   const response = await revenues.update({id: req.body.id, revenue: req.body.revenue, expenses: req.body.expenses}, {where: {id: req.body.id}}).then(() => {
-    return revenues.findOne({id: req.body.id})
+    return revenues.findOne({where: {id: req.body.id}})
   })
   if(response){
     res.send(response.dataValues)
